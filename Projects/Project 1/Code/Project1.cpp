3d18fb4c502f199711204ea2e4547e09b91fc92e
ThomasBolden <boldenth@msu.edu>
2016-02-11 13:35:16 -0500

    Initial commit

---
+++ Projects/Project 1/Code/Project1.cpp
@@ -0,0 +1,39 @@
+// Project 1 - Vector and Matrix Operations
+
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <iomanip>
+#include <string>
+//#include "armadillo"
+
+using namespace std;
+//using namespace arma;
+ofstream myfile;
+
+
+int main(){
+
+	// -~- Declaration of Variables -~- \\
+	double n;
+	string outfilename;
+
+	cout << "Enter a number: "; // user enters a number 
+	cin >> n;
+	cout << "Enter a name for the output file: "; // user enters a name for the output file
+	cin >> outfilename;
+
+	// body of the program
+
+
+
+	// writing value to file, to be read and graphed in python later
+    myfile.open(outfilename);
+	//myfile << setiosflags(ios::showpoint | ios::uppercase); // setting scientific notation of numbers
+	myfile << n << endl;
+
+    myfile.close();
+
+	return 0;
+
+}
